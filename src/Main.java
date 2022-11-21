97da4de34e8530c41f3547ae380f4d8414fcf7bb
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-21 15:31:52 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        double miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);
+    }
 }
